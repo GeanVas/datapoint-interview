4aaf8a78e9d382dad0797d7c3dfc24d513599e9c
Vasgan <gean.vasconez@gmail.com>
2026-02-21 21:52:05 -0500

    feat: improve migration & add seed

--- tasks-server/src/typeorm.config.ts
+++ tasks-server/src/typeorm.config.ts
@@ -1,9 +1,12 @@
 import { DataSource } from 'typeorm';
+import { UserOrmEntity } from './modules/users/infrastructure/user.orm-entity';
+import { TaskOrmEntity } from './modules/tasks/infrastructure/task.orm-entity';
+import { InitialMigration1771727876407 } from './migrations/1771727876407-InitialMigration';
 
 export default new DataSource({
   type: 'better-sqlite3',
   database: 'db.sqlite',
-  entities: [__dirname + '/../**/*.orm-entity.{js,ts}'],
-  migrations: [__dirname + '/../migrations/*.{js,ts}'],
+  entities: [UserOrmEntity, TaskOrmEntity],
+  migrations: [InitialMigration1771727876407],
   synchronize: false,
 });
